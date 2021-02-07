dd6fcfaf27e5cee1cac29256f4183fcad4ad80d6
suneetverma <71917610+suneetverma@users.noreply.github.com>
2021-02-07 22:47:17 +0530

    User validation added while surrendering the VM.

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ class addToDb(db.Model):
     virtmach = db.Column(db.Integer, primary_key=True)
     hostname = db.Column(db.String(20), nullable=False) 
     ip = db.Column(db.String(20), nullable=False) 
-    allocstatus = db.Column(db.String(20), nullable=False) 
+    allocstatus = db.Column(db.String(20), nullable=False)
+    user = db.Column(db.String(30), nullable=True)
+    timestamp = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
-        return f"serverResources(hostname = {hostname}, ip = {ip}, allocstatus = {allocstatus})"
+        #return f"serverResources(hostname = {hostname}, ip = {ip}, allocstatus = {allocstatus})"
+        return f"serverResources(hostname = {hostname}, ip = {ip}, allocstatus = {allocstatus}, user = {user}, timestamp = {timestamp})"
 
 db.create_all()
 
@@ -30,17 +33,30 @@ resPutArgs = reqparse.RequestParser()
 resPutArgs.add_argument('hostname', type=str, help="Hostname of the VM", required=True)
 resPutArgs.add_argument('ip', type=str, help="IP of the VM", required=True)
 resPutArgs.add_argument('allocstatus', type=str, help="Allocation status of the VM", required=True)
+resPutArgs.add_argument('user', type=str, help="IP of the VM", required=False)
+resPutArgs.add_argument('timestamp', type=str, help="IP of the VM", required=False)
+
+resGetArgs = reqparse.RequestParser()
+resGetArgs.add_argument('hostname', type=str, help="Hostname of the VM", required=False)
+resGetArgs.add_argument('ip', type=str, help="IP of the VM", required=False)
+resGetArgs.add_argument('allocstatus', type=str, help="Allocation status of the VM", required=False)
+resGetArgs.add_argument('user', type=str, help="IP of the VM", required=True)
+resGetArgs.add_argument('timestamp', type=str, help="IP of the VM", required=False)
 
 resUpdateArgs = reqparse.RequestParser()
 resUpdateArgs.add_argument('hostname', type=str, help='Hostname of the VM')
 resUpdateArgs.add_argument('ip', type=str, help='IP of the VM')
 resUpdateArgs.add_argument('allocstatus', type=str, help='Allocation status of the VM')
+resUpdateArgs.add_argument('user', type=str, help="IP of the VM", required=True)
+resUpdateArgs.add_argument('timestamp', type=str, help="IP of the VM", required=False)
 
 resource_fields = {
         'virtmach': fields.Integer,
         'hostname': fields.String,
         'ip': fields.String,
-        'allocstatus': fields.String
+        'allocstatus': fields.String,
+        'user': fields.String,
+        'timestamp': fields.Integer,
         }
 
 resourceDict={}
@@ -48,12 +64,14 @@ resourceDict={}
 class serverResources(Resource):
     @marshal_with(resource_fields)
     def get(self, vmid):
+        gargs = resGetArgs.parse_args()
         #result = addToDb.query.filter_by(allocstatus="Available").first()
         result = addToDb.query.filter_by(allocstatus="Available").first()
         if not result:
             logging.debug('Got allocation request, but none of the VMs available')
             abort(404, message='VMs not available for assignment, either all are reserved or down for maintenance')
         result.allocstatus = "Allocated"
+        result.user = gargs['user']
         db.session.commit()
         s1 = "Machine with IP " + str(result.ip) + " is marked Allocatted and assigned to customer at: \n" + str(time.ctime()) 
         logging.debug(s1)
@@ -77,7 +95,10 @@ class serverResources(Resource):
         result = addToDb.query.filter_by(ip=uargs['ip']).first()
         if not result:
             abort(404, message='the ip does not exist')
+        if uargs['user'] != result.user:
+            abort(404, message='You are not the same user whom machine was allocated')
         result.allocstatus = "Available"
+        result.user = None
         db.session.commit()
         s1 = "Machine with IP " + str(result.ip) + " is added back to the VM Resource Pool and marked Available at: \n" + str(time.ctime()) 
         logging.debug(s1)
--- test.py
+++ test.py
@@ -22,22 +22,26 @@ print(response.json())
 # Requesting a resource from the VM Resource pool
 # Returns details of the VM allocated from the Resourse Pool
 # Example: {'virtmach': 1, 'hostname': 'KVM-VM-001', 'ip': '1.1.1.1', 'allocstatus': 'Available'}
-response = requests.get(BASE + "virtualmachine/1")
+response = requests.get(BASE + "virtualmachine/1", {'user': 'USER1'})
 print(response.json())
-response = requests.get(BASE + "virtualmachine/2")
+response = requests.get(BASE + "virtualmachine/2", {'user': 'USER2'})
+#response = requests.get(BASE + "virtualmachine/2")
 print(response.json())
-response = requests.get(BASE + "virtualmachine/3")
+response = requests.get(BASE + "virtualmachine/3", {'user': 'USER3'})
+#response = requests.get(BASE + "virtualmachine/3")
 print(response.json())
-response = requests.get(BASE + "virtualmachine/4")
+response = requests.get(BASE + "virtualmachine/4", {'user': 'USER4'})
+#response = requests.get(BASE + "virtualmachine/4")
 print(response.json())
-response = requests.get(BASE + "virtualmachine/5")
+response = requests.get(BASE + "virtualmachine/5", {'user': 'USER5'})
+#response = requests.get(BASE + "virtualmachine/5")
 print(response.json())
 
 # Requesting a ALREADY ALLOCATED resource from the VM Resource pool
 # Returns message saying: {'message': 'VMs not available for assignment, either all are reserved or down for maintenance'}  
-response = requests.get(BASE + "virtualmachine/1")
+response = requests.get(BASE + "virtualmachine/1", {'user': 'USER99'})
 print(response.json())
-response = requests.get(BASE + "virtualmachine/5")
+response = requests.get(BASE + "virtualmachine/5", {'user': 'USER100'})
 print(response.json())
 
 # OAM task -  Delete VM from the Resource Pool
@@ -52,12 +56,18 @@ response = requests.delete(BASE + "virtualmachine/100")
 print(response.json())
 
 # FREEING TTHE RESOURCES: Returning the VM to the Resource Pool
-response = requests.patch(BASE + "virtualmachine/1", {"ip": "1.1.1.1"})
+response = requests.patch(BASE + "virtualmachine/1", {"ip": "1.1.1.1", 'user': "USER1"})
 print(response.json())
-response = requests.patch(BASE + "virtualmachine/2", {"ip": "2.2.2.2"})
+response = requests.patch(BASE + "virtualmachine/2", {"ip": "2.2.2.2", 'user': 'USER2'})
 print(response.json())
-response = requests.patch(BASE + "virtualmachine/3", {"ip": "3.3.3.3"})
+response = requests.patch(BASE + "virtualmachine/3", {"ip": "3.3.3.3", 'user': 'USER3'})
 print(response.json())
-response = requests.patch(BASE + "virtualmachine/4", {"ip": "4.4.4.4"})
+
+# Wrong User surrendering the machine:
+response = requests.patch(BASE + "virtualmachine/4", {"ip": "4.4.4.4", 'user': 'USER5'})
+print(response.json())
+
+# Right User surrendering the machine:
+response = requests.patch(BASE + "virtualmachine/4", {"ip": "4.4.4.4", 'user': 'USER4'})
 print(response.json())
 
